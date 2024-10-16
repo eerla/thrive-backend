5f3f2bd98b0fd73e0ebb60deb51fa0c039a156e2
eerla <gurubrahmam443@gmail.com>
2024-10-16 13:36:08 -0400

    Update notificationService.js

--- notifications/notificationService.js
+++ notifications/notificationService.js
@@ -9,7 +9,6 @@ const expo = new Expo();
 const fp_collection_id = process.env.FP_COLLECTION_ID;
 const td_collection_id = process.env.TD_COLLECTION_ID;
 
-// Function to send notifications to users
 async function sendNotificationsToUsers() {
     logger.info('Fetching users from PocketBase...');
     const users = await getAllRecords(td_collection_id);
@@ -21,9 +20,68 @@ async function sendNotificationsToUsers() {
         logger.info(msg);
         return msg;
     }
+
+    let sentCount = 0;
+    let failedToSendCount = 0;
+    let deliveredCount = 0;
+    let failedToDeliverCount = 0;
+
     const notificationPromises = await createNotificationPromises(users);
     const notificationResults = await Promise.allSettled(notificationPromises);
-    await handleNotificationResults(notificationResults);
+
+    // Count successfully sent and failed to send notifications
+    for (const result of notificationResults) {
+        if (result.status === 'fulfilled' && result.value) {
+            sentCount += result.value.length; // Assuming result.value is an array of tickets
+        } else {
+            failedToSendCount++;
+            // Save unsuccessful records to the database
+            logger.error(`Failed to send notification to token: ${result.value.user.token}. Reason: ${result.value.reason}`);
+            await createFailedRecord(fp_collection_id, {
+                token_x_user: result.value.user,
+                reason: result.value.reason
+            });
+        }
+    }
+
+    logger.info(`Notifications successfully sent: ${sentCount}`);
+    logger.info(`Notifications failed to send: ${failedToSendCount}`);
+
+    const successfulTickets = notificationResults
+        .filter(result => result.status === 'fulfilled' && result.value)
+        .flatMap(result => result.value);
+
+    if (successfulTickets.length > 0) {
+        const receiptResults = await processNotificationReceipts(successfulTickets);
+
+        // Count successfully delivered and failed to deliver notifications
+        receiptResults.forEach(result => {
+            if (result.status === 'ok') {
+                logger.info(`Notification delivered successfully!`);
+                deliveredCount++;
+            } else {
+                failedToDeliverCount++;
+
+                if (result.status === 'error') {
+                    logger.error(`Failed to send notification to token: ${result.token}. Reason: ${result.reason}`);
+        
+                    if (result.reason.includes('Invalid') || result.reason.includes('DeviceNotRegistered')) {
+                        // Token is invalid or device is not registered, remove the token from the database
+                        async function handleInvalidToken(result) {
+                            // Token is invalid or device is not registered, remove the token from the database
+                            await removeTokenFromDatabase(result.token);
+                            logger.info(`Removed invalid token: ${result.token}`);
+                        }
+
+                        handleInvalidToken(result);
+                    }
+                }
+            }
+        });
+    }
+
+    logger.info(`Notifications successfully delivered: ${deliveredCount}`);
+    logger.info(`Notifications failed to deliver: ${failedToDeliverCount}`);
 }
 
 // Create notification promises
@@ -81,29 +139,6 @@ async function sendMotivationalQuoteNotification(token, name, gender, age, occup
     return tickets; // Return tickets for further processing
 }
 
-// Function to handle notification results
-async function handleNotificationResults(results) {
-    const successfulTickets = [];
-    for (const result of results) {
-        if (result.status === 'fulfilled' && result.value) {
-            logger.info(`Notification sent successfully`);
-            successfulTickets.push(...result.value); // Collect successful tickets
-        } else {
-            logger.error(`Failed to send notification to token: ${result.value.user.token}. Reason: ${result.value.reason}`);
-            await createFailedRecord(fp_collection_id, {
-                token_x_user: result.value.user,
-                reason: result.value.reason
-            });
-        }
-    }
-    logger.info('Finished sending notifications.');
-
-    // Process receipts only for successfully sent notifications
-    if (successfulTickets.length > 0) {
-        logger.info('processing notifications receipts...')
-        await processNotificationReceipts(successfulTickets);
-    }
-}
 
 // Process notification receipts
 async function processNotificationReceipts(tickets) {
@@ -114,10 +149,10 @@ async function processNotificationReceipts(tickets) {
                 const receipt = await expo.getPushNotificationReceiptsAsync([receiptId]);
                 const status = receipt[receiptId].status;
                 if (status === 'ok') {
-                    logger.info(`Notification delived to device successfully`);
+                    logger.info(`Notification delivered successfully`);
                     return { status: 'ok', token: ticket.to };
                 } else if (status === 'error') {
-                    logger.error(`Error sending notification to device: ${ticket.to}. Reason: ${receipt[receiptId].message}`);
+                    logger.error(`Error delivering notification to token: ${ticket.to}. Reason: ${receipt[receiptId].message}`);
                     return {
                         status: 'error',
                         token: ticket.to,
@@ -134,27 +169,9 @@ async function processNotificationReceipts(tickets) {
         }
     });
 
-    const results = await Promise.all(receiptPromises);
-    await handleReceiptResults(results);
+    return await Promise.all(receiptPromises);
 }
 
-// Function to handle receipt results
-async function handleReceiptResults(results) {
-    logger.info('processing delivered promises to handle unregistered devices...')
-    for (const result of results) {
-        if (result.status === 'ok') {
-            logger.info(`Notification sent successfully!`);
-        } else if (result.status === 'error') {
-            logger.error(`Failed to send notification to token: ${result.token}. Reason: ${result.reason}`);
-
-            if (result.reason.includes('Invalid') || result.reason.includes('DeviceNotRegistered')) {
-                // Token is invalid or device is not registered, remove the token from the database
-                await removeTokenFromDatabase(result.token);
-                logger.info(`Removed invalid token: ${result.token}`);
-            }
-        }
-    }
-}
 
 async function removeTokenFromDatabase(token) {
     // Logic to remove the token from the database or mark it as inactive
